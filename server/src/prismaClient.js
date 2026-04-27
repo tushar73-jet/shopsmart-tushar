33e86e81544cf417323843b4625b74615cff0ba3
Tushar Kumar <184378329+tushar73-jet@users.noreply.github.com>
2026-04-27 18:14:32 +0530

    fix: supply better-sqlite3 Database handle to Prisma adapter

--- server/src/prismaClient.js
+++ server/src/prismaClient.js
@@ -3,8 +3,9 @@ const { PrismaBetterSqlite3 } = require('@prisma/adapter-better-sqlite3');
 const { PrismaClient } = require('@prisma/client');
 
 const connectionString = process.env.DATABASE_URL || 'file:./dev.db';
+const dbPath = connectionString.replace('file:', '');
 
-const db = new _Database('dev.db');
+const db = new _Database(dbPath);
 const adapter = new PrismaBetterSqlite3(db, { url: connectionString });
 const prisma = new PrismaClient({ adapter });
 
